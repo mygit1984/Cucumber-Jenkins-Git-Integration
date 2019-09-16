67e9c267264cc3543d811a87a2b3529713714414
PDC3A-Training.PDC3A <PDC3A-Training.PDC3A@P3A-HYSNXJ2.dir.svc.accenture.com>
2019-09-16 17:05:34 +0530

    1st commit

---
+++ CucumberProject/src/test/java/skeleton/CucumberRunner.java
@@ -0,0 +1,13 @@
+package skeleton;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"pretty","json:src/cucumber-reports/report1.json"},monochrome=true,
+features= {"src/test/resources/skeleton/login.feature"},dryRun=false)
+public class CucumberRunner {
+
+}
