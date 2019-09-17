f54e455d90ba6a599537340417ea5657c4012541
kannan.ramalingam <kannan.ramalingam@BDC4-L-5LW6QQ2.dir.svc.accenture.com>
2019-09-17 12:13:18 +0530

    2nd commit

--- CucumberProject/src/test/java/skeleton/LoginStepDefinition.java
+++ CucumberProject/src/test/java/skeleton/LoginStepDefinition.java
@@ -1,5 +1,6 @@
 package skeleton;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import junit.framework.Assert;
 
 public class LoginStepDefinition {
 	WebDriver driver;
--- CucumberProject/src/test/resources/skeleton/login.feature
+++ CucumberProject/src/test/resources/skeleton/login.feature
@@ -4,4 +4,4 @@ Given The URL of demo web shop application
 When User enters kannan2@gmail.com as username
 And User enters test1234 as password
 And User click on login button
-Then User login shall be success along with kannan1@gmail.com displayed in next page
+Then User login shall be success along with kannan2@gmail.com displayed in next page
